6e69319ec85278a1e63718d6e952b7d83a73be86
ant2alex <atlassian@firecat.de>
2018-04-17 09:48:17 +0200

    added upload to github releases

--- Scripts/post_build.sh
+++ Scripts/post_build.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 winname="${UNITYCI_PROJECT_NAME}_win.tar.gz"
-osxname="${UNITYCI_PROJECT_NAME}_osx.tar.gz"
+#osxname="${UNITYCI_PROJECT_NAME}_osx.tar.gz"
 
 # set file permissions so builds can run out of the box
 for infile in `find $(pwd)/Build | grep -E '\.exe$|\.dll$'`; do
@@ -10,7 +10,7 @@ done
 
 # tar and zip the build folders
 tar -C "$(pwd)/Build" -czvf $winname "windows"
-tar -C "$(pwd)/Build" -czvf $osxname "osx"
+#tar -C "$(pwd)/Build" -czvf $osxname "osx"
 
 # upload the tarballs to the server for storage
 #scp -i "${UPLOAD_KEYPATH}" \
